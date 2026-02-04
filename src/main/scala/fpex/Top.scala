8f459a8a4e5ff4d6a57a079dbaefa371729a9464
Shashank Anand <shashank.anand@berkeley.edu>
2026-02-03 17:16:35 -0800

    mill skeleton

---
+++ src/main/scala/fpex/Top.scala
@@ -0,0 +1,17 @@
+package fpex
+
+import chisel3._
+
+class TopIO extends Bundle {
+  val in  = Input(UInt(8.W))
+  val out = Output(UInt(8.W))
+}
+
+class Top extends Module {
+  val io = IO(new TopIO)
+  io.out := io.in
+}
+
+object Elaborate extends App {
+  circt.stage.ChiselStage.emitSystemVerilog(new Top)
+}
