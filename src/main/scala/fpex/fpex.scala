1dc458b81265b5ab42eaa83ce9b241770552cb6b
Shashank Anand <shashank.anand@berkeley.edu>
2026-02-04 17:32:57 -0800

    special cases: posneginf, nan, zero

--- src/main/scala/fpex/fpex.scala
+++ src/main/scala/fpex/fpex.scala
@@ -16,6 +16,46 @@ trait HasFPEXParams {
       case _ => (32, 8, 8)
     }
   }
+
+  // get signless exp,sig bits. NOTE: returns 1 fewer sig bit, prefix quiet/signalling bit
+  def constructNaNExpSig(fmt: FPFormat.Type): (UInt, UInt) = {
+    fmt match {
+      case FPFormat.FP32 => (Fill(8, 1.U(1.W)), Fill(22, 1.U(1.W)))
+      case FPFormat.FP16 => (Fill(5, 1.U(1.W)), Fill(9, 1.U(1.W)))
+      case FPFormat.BF16 => (Fill(8, 1.U(1.W)), Fill(6, 1.U(1.W)))
+      case _ => (Fill(8, 1.U(1.W)), Fill(22, 1.U(1.W)))
+    }
+  }
+
+  // get signless zero
+  def constructZero(fmt: FPFormat.Type): UInt = {
+    fmt match {
+      case FPFormat.FP32 => Cat(Fill(8, 0.U(1.W)), Fill(23, 0.U(1.W)))
+      case FPFormat.FP16 => Cat(Fill(5, 0.U(1.W)), Fill(10, 0.U(1.W)))
+      case FPFormat.BF16 => Cat(Fill(8, 0.U(1.W)), Fill(7, 0.U(1.W)))
+      case _ => Cat(Fill(8, 0.U(1.W)), Fill(23, 0.U(1.W)))
+    }
+  }
+
+  // get signless one
+  def constructOne(fmt: FPFormat.Type): UInt = {
+    fmt match {
+      case FPFormat.FP32 => Cat(127.U(8.W), 0.U(23.W))
+      case FPFormat.FP16 => Cat(15.U(5.W), 0.U(10.W))
+      case FPFormat.BF16 => Cat(127.U(8.W), 0.U(7.W))
+      case _ => Cat(127.U(8.W), 0.U(23.W))
+    }
+  }
+
+  // get signless infinity
+  def constructInf(fmt: FPFormat.Type): UInt = {
+    fmt match {
+      case FPFormat.FP32 => Cat(Fill(8, 1.U(1.W)), 0.U(23.W))
+      case FPFormat.FP16 => Cat(Fill(5, 1.U(1.W)), 0.U(10.W))
+      case FPFormat.BF16 => Cat(Fill(8, 1.U(1.W)), 0.U(7.W))
+      case _ => Cat(Fill(8, 1.U(1.W)), 0.U(23.W))
+    }
+  }
 }
 
 object FPEXState extends ChiselEnum {
@@ -29,7 +69,7 @@ class FPEXReq(wordWidth: Int, numLanes: Int, tagWidth: Int) extends Bundle {
   val tag = UInt(tagWidth.W)
   val neg = Bool() // 0 = e^x, 1 = e^-x
   val laneMask = UInt(numLanes.W)
-  val x = Vec(numLanes, UInt(wordWidth.W))
+  val xVec = Vec(numLanes, UInt(wordWidth.W))
 }
 
 class FPEXResp(wordWidth: Int, numLanes: Int, tagWidth: Int) extends Bundle {
@@ -42,6 +82,11 @@ class FPEX(fmt: FPFormat.Type, numLanes: Int = 4, tagWidth: Int = 1)
   extends Module
   with HasFPEXParams {
   val (wordWidth, expWidth, sigWidth) = getWordExpSigWidth(fmt)
+  val (naNExp, naNSig) = constructNaNExpSig(fmt)
+  val zero = constructZero(fmt)
+  val one = constructOne(fmt)
+  val infinity = constructInf(fmt)
+
   val io = IO(new Bundle {
     val req = Flipped(Decoupled(new FPEXReq(wordWidth, numLanes, tagWidth)))
     val resp = Decoupled(new FPEXResp(wordWidth, numLanes, tagWidth))
@@ -49,18 +94,44 @@ class FPEX(fmt: FPFormat.Type, numLanes: Int = 4, tagWidth: Int = 1)
 
   val state = RegInit(FPEXState.READY)
   val req = Reg(new FPEXReq(wordWidth, numLanes, tagWidth))
-  val recFnX = VecInit(io.req.bits.x.map(x => recFNFromFN(expWidth, sigWidth, x)))
+  val res = Reg(Vec(numLanes, UInt(wordWidth.W)))
+  val recFnVec = VecInit(io.req.bits.xVec.map(x => recFNFromFN(expWidth, sigWidth, x)))
+  val busy = state === FPEXState.BUSY
+
+  //stage 1: special case check and raw float decomposition
+  val rawFloatVec = VecInit(recFnVec.map(x => rawFloatFromRecFN(expWidth, sigWidth, x)))
+  val earlyRes = VecInit(rawFloatVec.zipWithIndex.map { case (x, i) =>
+    MuxCase(
+      0.U(wordWidth.W),
+      Seq(
+        x.isNaN -> Cat(x.sign, naNExp, isSigNaNRawFloat(x), naNSig),
+        x.isZero -> Cat(x.sign, one),
+        (x.isInf && x.sign) -> Cat(x.sign, zero),
+        (x.isInf && !x.sign) -> Cat(x.sign, infinity)
+      )
+    )
+  })
+  val earlyValid = VecInit(rawFloatVec.map(x => x.isInf || x.isZero || x.isInf))
+  val earlyTerminate = earlyValid.asUInt.andR
+
+  //stage 2
+  val stage2Valid = RegNext(busy && !earlyTerminate)
+
+  state := MuxCase(state, Seq(
+      (earlyTerminate && busy) -> FPEXState.DONE,
+      io.req.fire -> FPEXState.BUSY,
+      io.resp.fire -> FPEXState.READY
+    )
+  )
+  res := Mux(busy && earlyTerminate, earlyRes, res)
 
   io.req.ready := state === FPEXState.READY
   io.resp.valid := state === FPEXState.DONE
-  io.resp.bits.tag := 0.U
-  io.resp.bits.result := recFnX
-  io.resp.bits.laneMask := 0.U
+  io.resp.bits.tag := req.tag
+  io.resp.bits.result := res
+  io.resp.bits.laneMask := req.laneMask
 
   when (io.req.fire) {
-    state := FPEXState.BUSY
     req := io.req.bits
-  }.elsewhen(io.resp.fire) {
-    state := FPEXState.READY
   }
 }
